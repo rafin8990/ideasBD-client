8cfca3643fe7c4a0688891f84fe323dd9cf759e0
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-06-14 22:22:48 +0600

    footer completed

--- src/Shared/Container/Container.js
+++ src/Shared/Container/Container.js
@@ -1,7 +1,7 @@
 const Container = ({ children }) => {
     return (
         <div
-            className="max-w-[2520px] mx-auto lg:px-20 md:px-10 sm:px-2 px-4 bg-yellow-800">
+            className="max-w-[1500px] mx-auto lg:px-20 md:px-10 sm:px-2 px-4">
             {children}
         </div>
 
--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Container from "../Container/Container";
 
 const Navbar = () => {
   return (
