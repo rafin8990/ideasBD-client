42a3d6b22232adef9fcabbcf4d9f1545c9010a1a
rafin8990 <rafin8990@gmail.com>
2023-07-08 01:57:50 +0600

    code added

--- src/Pages/Home/Content/Content.js
+++ src/Pages/Home/Content/Content.js
@@ -1,10 +1,7 @@
 import React from "react";
-import RightLinks from "./RightLinks";
-import CallforPapers from "./CallforPapers";
-import Articles from "./Articles";
 import CurrentIssue from "./CurrentIssue";
-import MoreDetails from "./MoreDetails";
 import AboutTheJournal from "./RightLinksContent/AboutTheJournal";
+import CallforPapers from "./CallforPapers";
 
 const Content = () => {
   return (
@@ -12,17 +9,9 @@ const Content = () => {
       <div className="lg:flex">
         <div className="border">
           <img src="https://i.ibb.co/W20NzQs/5D-48370.jpg" alt="" />
-
           <CallforPapers></CallforPapers>
-          <CurrentIssue></CurrentIssue> 
+          <CurrentIssue></CurrentIssue>
           <AboutTheJournal></AboutTheJournal>
-          <Articles></Articles>
-          <MoreDetails></MoreDetails>
-        </div>
-
-
-
-        <div>
         </div>
       </div>
     </div>
